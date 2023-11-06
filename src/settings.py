3fcaae6f44e2b7fc966e70220651eb26ebf7aed4
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-11-06 22:15:58 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -69,7 +69,7 @@ ORGANIZATION_NAME: Optional[Union[str, List[str]]] = "Aalborg Univeristy, Denmar
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = "https://www.en.aau.dk/"
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
-SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = {"__PRETEXT__": "Additionally, images are tagged by 106 ***golf course*** groups. Explore them in the supervisely advanced labeling tool"}
+SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = {"__PRETEXT__": "Additionally, images are tagged by 106 ***golf course*** groups. Explore them in the supervisely labeling tool"}
 TAGS: Optional[List[str]] = None
 
 
